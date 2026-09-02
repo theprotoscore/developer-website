d531c41795d6418c39820b66de61379b25c6b865
Franklin <kubafranklin10@gmail.com>
2026-09-02 11:51:32 -0500

    Add icon and adjust colors

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const mobileNavigation = document.querySelector('.mobile-navigation');
 
-menuToggle.addEventListener('click', () => {
-    const isOpen = menuToggle.classList.toggle('is-open');
-
-    mobileNavigation.classList.toggle('is-open');
-
-    menuToggle.setAttribute('aria-expanded', isOpen);
-});
